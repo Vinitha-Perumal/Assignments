57fc9c1642156dcec313d6dcce4f3e45653de483
Vinitha Perumal <Vinitha.Perumal@trumpf.com>
2024-08-19 15:57:15 +0530

    changes on main branch sln

--- Assignment/Assignment.c
+++ Assignment/Assignment.c
@@ -1,4 +1,12 @@
+// ------------------------------------------------------------------------------------------------
+// Training ~ A training program for new joiners at Metamation, Batch - July 2024.
+// Copyright (c) Metamation India.
+// ------------------------------------------------------------------
+// Assignment.c
+// Program on main branch.
+// ------------------------------------------------------------------------------------------------
 #include <stdio.h>
+
 int main () {
 	printf ("Hello, world\n");
 	return 0;
