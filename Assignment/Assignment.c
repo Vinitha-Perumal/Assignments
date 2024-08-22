35d62e5ef0616bceec4155686efdabd2aba63ef2
Vinitha Perumal <Vinitha.Perumal@trumpf.com>
2024-08-22 12:46:29 +0530

    Revert "Merge pull request #3 from Vinitha-Perumal/A2.2"
    This reverts commit 77216d6259ee4dbd10afdb03d3ef03d9f6f73008, reversing
    changes made to 57fc9c1642156dcec313d6dcce4f3e45653de483.

---
+++ Assignment/Assignment.c
@@ -0,0 +1,13 @@
+// ------------------------------------------------------------------------------------------------
+// Training ~ A training program for new joiners at Metamation, Batch - July 2024.
+// Copyright (c) Metamation India.
+// ------------------------------------------------------------------
+// Assignment.c
+// Program on main branch.
+// ------------------------------------------------------------------------------------------------
+#include <stdio.h>
+
+int main () {
+	printf ("Hello, world\n");
+	return 0;
+}
