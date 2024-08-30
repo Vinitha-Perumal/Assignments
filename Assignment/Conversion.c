3241f7fdb93bc521f349d0fcfa532c9b1159ea1b
Vinitha Perumal <Vinitha.Perumal@trumpf.com>
2024-08-30 14:58:49 +0530

    A3: Modify bitlength in the code

--- Assignment/Conversion.c
+++ Assignment/Conversion.c
@@ -8,26 +8,32 @@
 
 #pragma warning(disable:4996)
 #include <stdio.h>
+
 void DecToBinary (int n);
 void DecToHexa (int n);
-
-// Function to convert the decimal number to binary
+int bitLength;
+/// <summary>Decimal number to binary Conversion</summary>
 void DecToBinary (int n) {
-   int binary[32];
-   int i = 0;
-   int flag = 0; // Flag is to check if the number is negative
-   if (n == 0)    printf ("binary is 0");
+   int binary[32], i = 0;
+   int isNegative = 0; // Flag is to check if the number is negative
+   if (n == 0)   printf ("0");  
    if (n < 0) {
       n = -n;
-      flag = 1;
+      isNegative = 1;
    }
    while (n > 0) {
       binary[i] = n % 2;
-      n = n / 2;
+      n /= 2;
       i++;
    }
    // If the number was negative,apply two's complement
-   if (flag) {
+   if (isNegative) {
+      if (i <= 7)   bitLength = 8;
+      else   bitLength = 16;
+      for (int j = i; j < bitLength; j++) {
+         binary[j] = 0;
+      }
+      i = bitLength;
       for (int j = 0; j < i; j++)   binary[j] = 1 - binary[j]; // Invert the bits for 2's complement
       int carry = 1;
       for (int j = 0; j < i; j++) {
@@ -41,35 +47,42 @@ void DecToBinary (int n) {
             break;
          }
       }
-      // If there is still a carry left, extend the binary array
-      if (carry == 1) {
-         binary[i] = 1;
-         i++;
-      }
    }
-   for (int k = i - 1; k >= 0; k--)    printf ("%d", binary[k]);   
+   else {
+      bitLength = (i <= 8) ? 8 : i;
+      for (int j = i; j < bitLength; j++) {
+         binary[j] = 0;
+      }
+      i = bitLength;
+   }
+   for (int k = i - 1; k >= 0; k--)   printf ("%d", binary[k]);   
 }
 
-// Function to convert the decimal number to Hexadecimal
+/// <summary>Decimal number to Hexadecimal Conversion</summary>
 void DecToHexa (int n) {
    char hexa[32];
    int i = 0;
-   if (n == 0)  printf ("Hexadecimal is 0");
+   if (n == 0)   printf ("0");  
    unsigned int num = (unsigned int)n;   // Handle negative numbers by using unsigned int
    while (num > 0) {
       int rem = num % 16;
       if (rem < 10)  hexa[i] = rem + '0';
       else  hexa[i] = rem - 10 + 'A';
-      num = num / 16;
+      num /= 16;
       i++;
    }
-   for (int k = i - 1; k >= 0; k--)    printf ("%c", hexa[k]);
+   int requiredDigits = (bitLength + 3) / 4;
+   while (i < requiredDigits) {
+      hexa[i] = '0';
+      i++;
+   }
+   for (int k = i - 1; k >= 0; k--)   printf ("%c", hexa[k]);
 }
 
 int main () {
    int number;
    printf ("Enter decimal Number: ");
-   scanf ("%d", &number);
+   scanf_s ("%d", &number);
    printf ("Binary: ");
    DecToBinary (number);
    printf ("\nHexadecimal: ");
