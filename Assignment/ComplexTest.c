bf952f0e599934f8f18078357219f8acf084c4a3
Vinitha Perumal <Vinitha.Perumal@trumpf.com>
2024-12-13 11:30:04 +0530

    Test1.2: changes made on TestCase() function

--- Assignment/ComplexTest.c
+++ Assignment/ComplexTest.c
@@ -18,29 +18,25 @@ void TestCases () {
    for (int i = 0; i < numOfTestCases; i++) {
       printf ("\nTestcase %d:\n\n", i + 1);
       // Addition TestCase
-      ComplexNumber expectedAddOut = { num1[i].Real + num2[i].Real,num1[i].Img + num2[i].Img },
-         addRes = Op_Add (num1[i], num2[i]);
-      printf ("Addition: %s\n", IsEqual (addRes, expectedAddOut) ? "Pass" : "Fail");
+      ComplexNumber expectedAddOut = { num1[i].Real + num2[i].Real,num1[i].Img + num2[i].Img };
+      printf ("Addition: %s\n", IsEqual (Op_Add (num1[i], num2[i]), expectedAddOut) ? "Pass" : "Fail");
 
       // Subtraction TestCase
-      ComplexNumber expectedSubOut = { num1[i].Real - num2[i].Real, num1[i].Img - num2[i].Img },
-         subRes = Op_Sub (num1[i], num2[i]);
-      printf ("Subtraction: %s\n", IsEqual (subRes, expectedSubOut) ? "Pass" : "Fail");
+      ComplexNumber expectedSubOut = { num1[i].Real - num2[i].Real, num1[i].Img - num2[i].Img };
+      printf ("Subtraction: %s\n", IsEqual (Op_Sub (num1[i], num2[i]), expectedSubOut) ? "Pass" : "Fail");
 
       // Multiplication TestCase
       ComplexNumber expectedMulOut = { (num1[i].Real * num2[i].Real) - (num1[i].Img * num2[i].Img),
-                                       (num1[i].Real * num2[i].Img) + (num1[i].Img * num2[i].Real) },
-         mulRes = Op_Mul (num1[i], num2[i]);
-      printf ("Multiplication: %s\n", IsEqual (mulRes, expectedMulOut) ? "Pass" : "Fail");
+                                       (num1[i].Real * num2[i].Img) + (num1[i].Img * num2[i].Real) };
+      printf ("Multiplication: %s\n", IsEqual (Op_Mul (num1[i], num2[i]), expectedMulOut) ? "Pass" : "Fail");
 
       // Conjugate TestCase
-      ComplexNumber expectedConjOut = { num1[i].Real, -num1[i].Img }, conjRes = Op_Conj (num1[i]);
-      printf ("Conjugate: %s\n", IsEqual (conjRes, expectedConjOut) ? "Pass" : "Fail");
+      ComplexNumber expectedConjOut = { num1[i].Real, -num1[i].Img };
+      printf ("Conjugate: %s\n", IsEqual (Op_Conj (num1[i]), expectedConjOut) ? "Pass" : "Fail");
 
       // Modulus TestCase
-      double expectedModOut = sqrt (num1[i].Real * num1[i].Real + num1[i].Img * num1[i].Img),
-         modRes = Op_Mod (num1[i]);
-      printf ("Modulus: %s\n", modRes == expectedModOut ? "Pass" : "Fail");
+      double expectedModOut = sqrt (num1[i].Real * num1[i].Real + num1[i].Img * num1[i].Img);
+      printf ("Modulus: %s\n", Op_Mod (num1[i]) == expectedModOut ? "Pass" : "Fail");
    }
 }
 
